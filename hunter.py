379b5dfaea3fb56c87d2ba60aa3934d6817a7de9
Aghouiles_Naoual <aghouilesnaoual0@gmail.com>
2022-02-08 11:52:08 +0100

    les tests

--- hunter.py
+++ hunter.py
@@ -1,54 +1,62 @@
-from msilib.schema import Property
+
 import random
-from xml.sax.handler import property_encoding
+
 
 class hunter:
 
 
-# instance attribute
- def __init__(self):
-   self.__degat = random.randint(1,2)
-   self.__chance=10
-   self.__fuite=20
-   self.__prix=25
-   self.__type_unite="hunter"
-   
-@property
-def degat(self):
-    """The foo property."""
-    return self.__degat
-@degat.setter
-def degat(self, value):
-    self.__degat = value
+  # instance attribute
+    def __init__(self):
+      self.__degat = random.randint(1,2)
+      self.__chance=10
+      self.__fuite=20
+      self.__prix=25
+      self.__type_unite="hunter"
     
-@property
-def chance(self):
-    """The chance property."""
-    return self.__chance
-@chance.setter
-def chance(self, value):
-    self.__chance = value
+    @property
+    def degat(self):
+      return self.__degat
     
-@property
-def fuite(self):
-    """The fuite property."""
-    return self.__fuite
-@fuite.setter
-def fuite(self, value):
-    self.__fuite = value
+    @degat.setter
+    def degat(self, value):
+      self.__degat = value
+      
+    @property
+    def chance(self):
+        return self.__chance
+      
+    @chance.setter
+    def chance(self, value):
+        self.__chance = value
+        
+    @property
+    def fuite(self):
+      return self.__fuite
     
-@property
-def prix(self):
-    """The prix property."""
-    return self.__prix
-@prix.setter
-def prix(self, value):
-    self.__prix = value
+    @fuite.setter
+    def fuite(self, value):
+      self.__fuite = value
     
-@property
-def type_unite(self):
-    """The type_unite property."""
-    return self.__type_unite
-# @type_unite.setter
-# def type_unite(self, value):
-#     self.__type_unite = value
+    @property
+    def prix(self):
+    
+      return self.__prix
+    
+    @prix.setter
+    def prix(self, value):
+      self.__prix = value
+    
+    @property
+    def type_unite(self):
+    
+      return self.__type_unite
+  
+    @type_unite.setter
+    def type_unite(self, value):
+      self.__type_unite = value
+
+
+if __name__ == "__main__":
+    
+    h = hunter()
+    print(h.degat)
