aca558f4adac5f08f38d0254f1fd74d6f7993f58
Aghouiles_Naoual <aghouilesnaoual0@gmail.com>
2022-02-08 11:13:06 +0100

    Creation attr

---
+++ hunter.py
@@ -0,0 +1,16 @@
+import random
+
+class hunter:
+
+
+# instance attribute
+ def __init__(self, degat , chance,fuite ,prix, type_unite):
+   degat = random.randint(1,2)
+   chance=10
+   fuite=20
+   prix=25
+   type_unite="hunter"
+   
+
+    
+    
