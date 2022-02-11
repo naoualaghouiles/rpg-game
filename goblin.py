c93992ae080f3741e9c6f9d488b2120133e28bc7
Aghouiles_Naoual <aghouilesnaoual0@gmail.com>
2022-02-11 13:34:38 +0100

    les getters et les setters des attr

--- goblin.py
+++ goblin.py
@@ -5,4 +5,20 @@ class goblin:
     def __init__(self):
         self.__degat = random.randint(2,3)
         self.__loot = random.choice([1,1.5])
+        
+    @property
+    def degat(self):
+        """The degat property."""
+        return self.__degat
+    @degat.setter
+    def degat(self, value):
+        self.__degat = value
+        
+    @property
+    def loot(self):
+        """The loot property."""
+        return self.__loot
+    @loot.setter
+    def loot(self, value):
+        self.__loot = value
     
